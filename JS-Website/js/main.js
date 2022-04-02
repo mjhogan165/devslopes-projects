f95c460b9ae1837d6ab2f91adbf4ae6e45d8ec1d
Matthew Hogan <69777851+mjhogan165@users.noreply.github.com>
2022-04-02 18:33:17 -0400

    hero css

--- JS-Website/js/main.js
+++ JS-Website/js/main.js
@@ -4,8 +4,8 @@ const isVisible = 'is-visible'
 
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
-
-for (elm of openModal) {
+//full site modal open buttons
+for (const elm of openModal) {
     elm.addEventListener('click', function(){
          const modalID = this.dataset.open;
          document.getElementById(modalID).classList.add(isVisible);
